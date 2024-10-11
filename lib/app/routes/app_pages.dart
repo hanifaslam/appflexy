7a71ae176cac0b7e7a6bad756904a712d4c49991
SabillillahX <sabillillahxtkj@gmail.com>
2024-10-11 07:22:18 +0700

    profile

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,11 +1,11 @@
 import 'package:get/get.dart';
 
-import '../modules/coba/bindings/coba_binding.dart';
-import '../modules/coba/views/coba_view.dart';
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
 import '../modules/login/bindings/login_binding.dart';
 import '../modules/login/views/login_view.dart';
+import '../modules/profile/bindings/profile_binding.dart';
+import '../modules/profile/views/profile_view.dart';
 
 part 'app_routes.dart';
 
@@ -26,9 +26,9 @@ class AppPages {
       binding: LoginBinding(),
     ),
     GetPage(
-      name: _Paths.COBA,
-      page: () => const CobaView(),
-      binding: CobaBinding(),
+      name: _Paths.PROFILE,
+      page: () => const ProfileView(),
+      binding: ProfileBinding(),
     ),
   ];
 }
