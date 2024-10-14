af2730fb452d3fd4b3e690c97cdb54ec248bbfc6
hanifaslamm <aslamhanif141@gmail.com>
2024-10-14 15:01:55 +0700

    navbar blm done

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    // return GetMaterialApp(
     //   debugShowCheckedModeBanner: false,
     //   title: "Application",
     //   initialRoute: Routes.HOME,
