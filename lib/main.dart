4332d4202b9fd5f1672b5dc50760c6440539f0aa
hanifaslamm <aslamhanif141@gmail.com>
2024-10-15 13:21:42 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     // return GetMaterialApp(
     //   debugShowCheckedModeBanner: false,
     //   title: "Application",
-    //   initialRoute: Routes.TAMBAH_PRODUK,
+    //   initialRoute: Routes.HOME,
     //   getPages: AppPages.routes,
     // );
     return FutureBuilder(
